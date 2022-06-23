4c1a434263f073f8db8efcd5006af63d2e373284
zjlian <zjlianzjlianzjlian@outlook.com>
2022-06-23 22:46:29 +0800

    feat: SDS

---
+++ base/simple_dynamic_string.hpp
@@ -0,0 +1,179 @@
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <cstring>
+#include <memory>
+#include <string_view>
+#include <vector>
+
+class SimpleDynamicString
+{
+public:
+    /// 默认构造空的 SDS
+    SimpleDynamicString() = default;
+
+    /// 从指定内存地址构造指定长度的 SDS
+    SimpleDynamicString(const char *init, size_t init_length)
+        : length_(init_length)
+    {
+        assert(init_length >= 0);
+
+        if (length_ > 0)
+        {
+            // 分配的内存比指定字符串多一个字节，存放 '\n'
+            buffer_ = std::make_unique<char[]>(length_ + 1);
+            assert(buffer_ != nullptr && "Out Of Memory");
+
+            if (init != nullptr)
+            {
+                std::copy_n(init, length_, buffer_.get());
+                buffer_[length_] = '\0';
+            }
+            else
+            {
+                std::fill_n(buffer_.get(), length_, '\0');
+            }
+        }
+    }
+
+    /// 从 C-Style 字符串构造 SDS
+    SimpleDynamicString(const char *init)
+    {
+        auto init_length = (init == nullptr) ? 0 : std::strlen(init);
+        SimpleDynamicString(init, init_length);
+    }
+
+    /// 复制构造
+    SimpleDynamicString(const SimpleDynamicString &other)
+    {
+        CopyFrom(other);
+    }
+
+    /// 移动构造
+    SimpleDynamicString(SimpleDynamicString &&other)
+    {
+        Swap(other);
+        other.length_ = 0;
+        other.free_ = 0;
+        other.buffer_ = nullptr;
+    }
+
+    SimpleDynamicString &operator=(const SimpleDynamicString &other)
+    {
+        CopyFrom(other);
+        return *this;
+    }
+
+    SimpleDynamicString &operator=(SimpleDynamicString &&other)
+    {
+        Swap(other);
+        other.length_ = 0;
+        other.free_ = 0;
+        other.buffer_ = nullptr;
+        return *this;
+    }
+
+    /// 获取 SDS 的长度
+    size_t Length()
+    {
+        return length_;
+    }
+
+    /// 剩余未使用的容量
+    size_t Avail()
+    {
+        return free_;
+    }
+
+    /// TODO: 刷新长度数据，重新计算实际字符串的长度
+    void UpdateLength();
+
+    /// 追加容量
+    void MakeRoomFor(size_t append_length)
+    {
+        assert(append_length >= 0);
+
+        if (Avail() >= append_length)
+        {
+            return;
+        }
+
+        // 简单两倍扩容
+        auto new_length = (Length() + append_length) * 2;
+        auto new_buffer = std::make_unique<char[]>(new_length + 1);
+        assert(buffer_ != nullptr && "Out Of Memory");
+        std::copy_n(buffer_.get(), Length(), new_buffer.get());
+        new_buffer[Length()] = '\0';
+
+        std::swap(buffer_, new_buffer);
+        free_ = new_length - Length();
+    }
+
+    /// 追加内容，从指定内存地址读取指定长度的数据追加到当前 SDS 后面
+    void Append(const char *target, size_t length);
+
+    /// 追加内容，追加 C-Styley 字符串到当前 SDS 后面
+    void Append(const char *target);
+
+    /// 追加内容，追加 string_view 的内容到当前 SDS 后面
+    void Append(std::string_view target);
+
+    /// 追加内容，将另一个 SDS 追加到当前 SDS 后面
+    void Append(const SimpleDynamicString &other);
+
+    /// 删除两端的指定字符
+    /// TODO: 提供 string_view 版本
+    void Trim(const char *c);
+
+    /// 保留指定区间内的内容
+    /// TODO: 提供 string_view 版本
+    void Range(int64_t start, int64_t end);
+
+    /// 全转小写
+    void ToLower();
+
+    /// 全转大写
+    void ToUpper();
+
+    /// 分割
+    std::vector<std::string_view> Split(std::string_view separator);
+
+private:
+    /// 复制构造和赋值的实现函数
+    void CopyFrom(const SimpleDynamicString &other)
+    {
+        length_ = other.length_;
+        free_ = other.free_;
+
+        if (length_ > 0)
+        {
+            assert(other.buffer_ != nullptr);
+
+            buffer_ = std::make_unique<char[]>(length_ + 1);
+            assert(buffer_ != nullptr && "Out Of Memory");
+
+            std::copy_n(other.buffer_.get(), length_, buffer_.get());
+        }
+        else
+        {
+            length_ = 0;
+            free_ = 0;
+            buffer_ = nullptr;
+        }
+    }
+
+    /// 交换两个 SDS 的内容
+    void Swap(SimpleDynamicString &other)
+    {
+        std::swap(length_, other.length_);
+        std::swap(free_, other.free_);
+        std::swap(buffer_, other.buffer_);
+    }
+
+private:
+    int64_t length_ = 0;
+    int64_t free_ = 0;
+    std::unique_ptr<char[]> buffer_ = nullptr;
+};
