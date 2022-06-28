87370cdfe5c1f6bb80604fa41417917ce3e5518b
winily <18934086807@163.com>
2022-06-28 15:27:24 +0800

    feat: CMakeLists add fcoroutines

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include "base/hello.h"
 
 #include <iostream>
+#include <coroutine>
+
+
+
+struct promise;
+struct coroutine : std::coroutine_handle<promise>
+{ using promise_type = struct promise; };
+ 
+struct promise {
+  coroutine get_return_object()
+  { return {coroutine::from_promise(*this)}; }
+  std::suspend_always initial_suspend() noexcept { return {}; }
+  std::suspend_always final_suspend() noexcept { return {}; }
+  void return_void() {}
+  void unhandled_exception() {}
+};
+
+void bad3() {
+  coroutine h = [i = 0]() -> coroutine { // a lambda that's also a coroutine
+    std::cout << i;
+    co_return;
+  }(); // immediately invoked
+  // lambda destroyed
+  h.resume(); // uses (anonymous lambda type)::i after free
+  h.destroy();
+}
 
 int main(int, char **)
 {
-    std::cout << HelloMessage() << std::endl;
+
+    bad3();
+std::cout << HelloMessage() << std::endl;
 }
--- script/run.sh
+++ script/run.sh
@@ -5,6 +5,7 @@ fi
 mkdir ./build
 cd build
 cmake ..
-make -j
+cpu=$(cat /proc/cpuinfo | grep processor | wc -l)
+make -j ${cpu}
 
 ./toy_redis
