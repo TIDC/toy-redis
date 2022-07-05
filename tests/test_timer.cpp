5d9181f86a9b7c69605db1c9d4fd912f7ee1516b
zjlian <zjlianzjlianzjlian@outlook.com>
2022-07-05 01:49:55 +0000

    fix: 修改 TImer 测试用例

--- tests/test_timer.cpp
+++ tests/test_timer.cpp
@@ -5,8 +5,9 @@
 #include <cmath>
 #include <cstdint>
 #include <thread>
+#include <unistd.h>
 
-TEST(io_service, SelectPoller)
+TEST(io_service, Timer)
 {
     net::IOService ios;
     int64_t last_call_say_hello = 0;
@@ -18,6 +19,7 @@ TEST(io_service, SelectPoller)
             // 简单场景下定时器误差小于 1 ms
             auto diff = std::abs(300000 - (now - last_call_say_hello));
             std::cout << "周期定时器误差 " << diff << " us" << std::endl;
+            // 'L' 小于，'E' 等于
             EXPECT_LE(diff, 1000);
         }
         std::cout << "hello~~ " << base::NowMilliseconds() << std::endl;
@@ -26,11 +28,22 @@ TEST(io_service, SelectPoller)
     auto t1 = ios.SetInterval(say_hello, 300);
     ASSERT_EQ(t1, 0);
 
-    auto quit = [] { exit(0); };
+    auto quit = [&] {
+        ios.Stop();
+    };
     auto t2 = ios.SetTimeout(quit, 1000);
     ASSERT_EQ(t2, 1);
 
     std::thread([&] {
         ios.Run();
     }).join();
+}
+
+TEST(io_service, EmptyTimer)
+{
+    net::IOService ios;
+    std::thread([&] {
+        ios.Run();
+    }).detach();
+    sleep(1);
 }
