cee3478e2d59a5f4712c74747eaa80da5041d39a
zjlian <zjlianzjlianzjlian@outlook.com>
2022-07-17 02:14:00 +0800

    fix: 禁止通过通用迭代器类型擦除容器修改迭代器引用的元素

--- toy-redis/iterator.hpp
+++ toy-redis/iterator.hpp
@@ -4,6 +4,7 @@
 
 #include <memory>
 #include <type_traits>
+#include <utility>
 
 /// 该文件实现了通用迭代器的类型消除容器
 
@@ -19,7 +20,7 @@ namespace tr
             virtual ~IteratorInterface() = default;
 
             /// 获取迭代器指向的元素的引用
-            virtual T &Get() = 0;
+            virtual const T &Get() = 0;
 
             /// 下一个元素
             virtual IteratorInterface &Next() = 0;
@@ -45,12 +46,12 @@ namespace tr
         public:
             using ValueType = typename T::value_type;
 
-            explicit IteratorImplement(T &target)
-                : target_(target)
+            explicit IteratorImplement(T target)
+                : target_(std::move(target))
             {
             }
 
-            ValueType &Get() override
+            const ValueType &Get() override
             {
                 return *target_;
             }
@@ -69,8 +70,7 @@ namespace tr
 
             bool Equal(const IteratorInterface<ValueType> &other) override
             {
-                return dynamic_cast<const IteratorImplement &>(other).target_ ==
-                       target_;
+                return ((const IteratorImplement &)(other)).target_ == target_;
             }
 
         private:
@@ -84,13 +84,14 @@ namespace tr
     {
     public:
         template <typename T>
-        explicit Iterator(T iterator)
+        explicit Iterator(T &&iterator)
         {
-            using IteratorType = std::remove_cvref_t<T>;
-            impl_ = std::make_unique<IteratorImplement<IteratorType>>(iterator);
+            using Type = std::remove_reference_t<T>;
+            impl_ = std::make_unique<IteratorImplement<Type>>(
+                std::forward<T>(iterator));
         }
 
-        ValueType &Get()
+        const ValueType &Get()
         {
             return impl_->Get();
         }
